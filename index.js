6b6e1354b43a72de1247837f0aaca893deff940a
Karilyn Kempton <karilyn.kempton@gmail.com>
2022-11-30 18:07:15 +0000

    establishing base case for recursion problem

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-
 function sum(fromN, toN) {
+  if (fromN === toN) { // this is the base case
+    return toN;
+  };
+ }
+
   // Sum all the values from fromN up to toN
-}
 
 module.exports = sum;
+
+
+/* base case: the function will not need to call itself if
+fromN === toN -> just return the value of toN because there's nothing to sum
+if fromN(3), toN(4) -> 3 + 4 or fromN + toN
+if fromN(3), toN(4) -> 3 + 4 + 5
+if fromN(3), toN(6) -> 3 + 4 + 5 + 6
+if fromN(3), toN(7) -> 3 + 4 + 5 + 6 + 7
+*/
