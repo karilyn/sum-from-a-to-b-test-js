a7c2cf546682677509bc63cb411c255cd9c5c2fb
Karilyn Kempton <karilyn.kempton@gmail.com>
2023-01-25 22:50:46 +0000

    Solve recursion stretch activity

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ function sum(fromN, toN) {
   if (fromN === toN) { // this is the base case
     return toN;
   };
+  return toN + sum(fromN, toN - 1)
  }
 
-  // Sum all the values from fromN up to toN
+console.log(sum(3, 7))
 
 module.exports = sum;
 
 
-/* base case: the function will not need to call itself if
+/* 
+base case: the function will not need to call itself if
 fromN === toN -> just return the value of toN because there's nothing to sum
-if fromN(3), toN(4) -> 3 + 4 or fromN + toN
-if fromN(3), toN(4) -> 3 + 4 + 5
-if fromN(3), toN(6) -> 3 + 4 + 5 + 6
-if fromN(3), toN(7) -> 3 + 4 + 5 + 6 + 7
+if fromN(3), toN(4) -> 4 + 3 or fromN + toN
+if fromN(3), toN(5) -> 5 + 4 + 3 (5 + (3 + 5 - 1))
+if fromN(3), toN(6) -> 6 + 5 + 4 + 3 (6 + (3 + 4 + 6 - 1))
+if fromN(3), toN(7) -> 7 + 6 + 5 + 4 + 3
 */
